1975abbaf0e06a92dc4767a664776ceaf52665ef
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-08-04 21:45:20 +0530

    added resources of Adobe Photoshop

--- GetPlaced/settings.py
+++ GetPlaced/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'v(s_1s5r9!4xcx++v9s62ha#)xl$&h6=g-r+02x2@u6fmu8w(0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','192.168.43.35']
 
 
 # Application definition
