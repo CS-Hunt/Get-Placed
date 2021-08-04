d2a00aba2525b88c750938ca456f25e16ed960fb
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-08-04 18:37:25 +0530

    Feature and updates related to styling

--- app/static/app/js/app.js
+++ app/static/app/js/app.js
@@ -34,9 +34,27 @@ menuCloseBtn.onclick = function () {
 $(document).ready(function () {
     $('#autoWidth').lightSlider({
         autoWidth: true,
-        loop: true,
+        // loop: true,
         onSliderLoad: function () {
             $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+});
+$(document).ready(function () {
+    $('#autoWidth1').lightSlider({
+        autoWidth: true,
+        // loop: true,
+        onSliderLoad: function () {
+            $('#autoWidth1').removeClass('cS-hidden');
+        }
+    });
+});
+$(document).ready(function () {
+    $('#autoWidth2').lightSlider({
+        autoWidth: true,
+        // loop: true,
+        onSliderLoad: function () {
+            $('#autoWidth2').removeClass('cS-hidden');
         }
     });
 });
--- app/static/app/js/lightslider.js
+++ app/static/app/js/lightslider.js
@@ -37,12 +37,12 @@
         swipeThreshold: 40,
         responsive: [],
         /* jshint ignore:start */
-        onBeforeStart: function ($el) {},
-        onSliderLoad: function ($el) {},
-        onBeforeSlide: function ($el, scene) {},
-        onAfterSlide: function ($el, scene) {},
-        onBeforeNextSlide: function ($el, scene) {},
-        onBeforePrevSlide: function ($el, scene) {}
+        onBeforeStart: function ($el) { },
+        onSliderLoad: function ($el) { },
+        onBeforeSlide: function ($el, scene) { },
+        onAfterSlide: function ($el, scene) { },
+        onBeforeNextSlide: function ($el, scene) { },
+        onBeforePrevSlide: function ($el, scene) { }
         /* jshint ignore:end */
     };
     $.fn.lightSlider = function (options) {
@@ -378,7 +378,7 @@
                         }
                     }
                     var $cSouter = $slide.parent();
-                    $cSouter.find('.lSPager').html(pagers); 
+                    $cSouter.find('.lSPager').html(pagers);
                     if (settings.gallery === true) {
                         if (settings.vertical === true) {
                             // set Gallery thumbnail width
@@ -447,12 +447,12 @@
                 };
                 setCss();
                 if (obj.find('img').length) {
-                    if ( obj.find('img')[0].complete) {
+                    if (obj.find('img')[0].complete) {
                         setCss();
                         if (!interval) {
                             $this.auto();
-                        }   
-                    }else{
+                        }
+                    } else {
                         obj.find('img').on('load', function () {
                             setTimeout(function () {
                                 setCss();
@@ -462,7 +462,7 @@
                             }, 100);
                         });
                     }
-                }else{
+                } else {
                     if (!interval) {
                         $this.auto();
                     }
@@ -612,14 +612,14 @@
             slideThumb: function () {
                 var position;
                 switch (settings.currentPagerPosition) {
-                case 'left':
-                    position = 0;
-                    break;
-                case 'middle':
-                    position = (elSize / 2) - (thumbWidth / 2);
-                    break;
-                case 'right':
-                    position = elSize - thumbWidth;
+                    case 'left':
+                        position = 0;
+                        break;
+                    case 'middle':
+                        position = (elSize / 2) - (thumbWidth / 2);
+                        break;
+                    case 'right':
+                        position = elSize - thumbWidth;
                 }
                 var sc = scene - $el.find('.clone.left').length;
                 var $pager = $slide.parent().find('.lSPager');
@@ -647,15 +647,15 @@
                     }, settings.pause);
                 }
             },
-            pauseOnHover: function(){
+            pauseOnHover: function () {
                 var $this = this;
                 if (settings.auto && settings.pauseOnHover) {
-                    $slide.on('mouseenter', function(){
+                    $slide.on('mouseenter', function () {
                         $(this).addClass('ls-hover');
                         $el.pause();
                         settings.auto = true;
                     });
-                    $slide.on('mouseleave',function(){
+                    $slide.on('mouseleave', function () {
                         $(this).removeClass('ls-hover');
                         if (!$slide.find('.lightSlider').hasClass('lsGrabbing')) {
                             $this.auto();
@@ -874,11 +874,11 @@
                     }
                 }
 
-                $(window).on('focus', function(){
+                $(window).on('focus', function () {
                     $this.auto();
                 });
-                
-                $(window).on('blur', function(){
+
+                $(window).on('blur', function () {
                     clearInterval(interval);
                 });
 
@@ -927,7 +927,7 @@
                 if (settings.mode === 'slide') {
                     if (settings.vertical === false) {
                         plugin.setHeight($el, false);
-                    }else{
+                    } else {
                         plugin.auto();
                     }
                 } else {
@@ -1080,7 +1080,7 @@
                 }
             }
             return sc + 1;
-        }; 
+        };
         $el.getTotalSlideCount = function () {
             return $slide.find('.lslide').length;
         };
@@ -1097,18 +1097,18 @@
         };
         $el.destroy = function () {
             if ($el.lightSlider) {
-                $el.goToPrevSlide = function(){};
-                $el.goToNextSlide = function(){};
-                $el.mode = function(){};
-                $el.play = function(){};
-                $el.pause = function(){};
-                $el.refresh = function(){};
-                $el.getCurrentSlideCount = function(){};
-                $el.getTotalSlideCount = function(){};
-                $el.goToSlide = function(){}; 
+                $el.goToPrevSlide = function () { };
+                $el.goToNextSlide = function () { };
+                $el.mode = function () { };
+                $el.play = function () { };
+                $el.pause = function () { };
+                $el.refresh = function () { };
+                $el.getCurrentSlideCount = function () { };
+                $el.getTotalSlideCount = function () { };
+                $el.goToSlide = function () { };
                 $el.lightSlider = null;
                 refresh = {
-                    init : function(){}
+                    init: function () { }
                 };
                 $el.parent().parent().find('.lSAction, .lSPager').remove();
                 $el.removeClass('lightSlider lSFade lSSlide lsGrab lsGrabbing leftEnd right').removeAttr('style').unwrap().unwrap();
