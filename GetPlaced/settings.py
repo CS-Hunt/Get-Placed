4566278ebc294c90747b97fe45f0c1d467d352a3
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-08-10 13:01:22 +0530

    added to heroku

--- GetPlaced/settings.py
+++ GetPlaced/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['getplaced.herokuapp.com','localhost','192.168.43.35']
 
