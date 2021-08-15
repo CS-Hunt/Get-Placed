9a66c01a82c08ced6c728ce43aaa351230ba5673
Nitesh Singh <ns98738698@gmail.com>
2021-08-15 14:02:45 +0000

    added resources to resources section

--- app/views.py
+++ app/views.py
@@ -14,31 +14,18 @@ class HomeView(ListView):
     template_name = 'app/home.html'
     ordering = ['-post_date']
     context_object_name = "Placement_Company_Detail_list"    #default is object_list as well as model's_verbose_name_list and/or model's_verbose_name_plural_list, if defined in the model's inner Meta class
-    paginate_by = 10  
+    paginate_by = 10
 
-    # def get_context_data(self,*args,**kwargs):
-    #     cat_menu = Category.objects.all()
-    #     context = super(HomeView, self).get_context_data(*args,**kwargs)
-    #     context["cat_menu"] = cat_menu
-    #     return context
-
-    # def get_context_data(self,*args,**kwargs):
-    #     # users = Profile.objects.all()
-    #     context = super(HomeView, self).get_context_data(*args,**kwargs)
-
-    #     page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
-    #     context["page_user"] = page_user
-    #     return context
 
 def AboutView(request):
-    return render(request, 'app/About.html')   
+    return render(request, 'app/About.html')
 
 def TermsAndCondition(request):
-    return render(request, 'app/Terms&Condition.html') 
+    return render(request, 'app/Terms&Condition.html')
 
 def JoinUsView(request):
-    return render(request, 'app/join_us.html')  
-    
+    return render(request, 'app/join_us.html')
+
 class CompanyDetailView(DetailView):
     model = Placement_Company_Detail
     template_name = 'app/Company_details.html'
@@ -47,15 +34,6 @@ class CompanyDetailView(DetailView):
         context = super(CompanyDetailView, self).get_context_data(*args,**kwargs)
 
         stuff = get_object_or_404(Placement_Company_Detail, id=self.kwargs['pk'])
-        # total_likes = stuff.total_likes() 
-
-        # liked = False 
-        # if stuff.likes.filter(id=self.request.user.id).exists():
-        #     liked= True
-
-        # context["cat_menu"] = cat_menu
-        # context["total_likes"] = total_likes
-        # context['liked'] = liked
         return context
 
 class AddJobView(CreateView):
@@ -70,7 +48,7 @@ class UpdateJobPostView(UpdateView):
     form_class = Edit_Post_Form
     template_name = 'app/update_job_post.html'
     # fields = ['title','title_tag','body']
-    
+
 class DeleteJobPostView(DeleteView):
     model = Placement_Company_Detail
     template_name = 'app/delete_post.html'
@@ -84,15 +62,15 @@ class UserLogin(LoginView):
 class CreateProfilePageView(CreateView):
     model = Profile
     form_class = ProfilePageView
-    template_name = 'account/craete_user_profile_page.html' 
+    template_name = 'account/craete_user_profile_page.html'
     # fields = '__all__'
     success_url = reverse_lazy('home')
-    
+
 
     def form_valid(self,form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-    
+
 class EditProfilePageView(UpdateView):
     model = Profile
     form_class = EditProfileFormPage
@@ -119,17 +97,6 @@ class ShowProfileView(DetailView):
         return context
 
 
-# class ShowPostInPorfile(DetailView):
-#     model = StudentBlogModel
-#     template_name = 'account/user_profile.html'
-
-#     def get_context_data(self,*args,**kwargs):
-#         # users = Profile.objects.all()
-#         context = super(ShowPostInPorfile, self).get_context_data(*args,**kwargs)
-
-#         page_user_post = get_object_or_404(StudentBlogModel, id=self.kwargs['pk'])
-#         context["page_user_post"] = page_user_post
-#         return context
 
 class UserEditView(UpdateView):
     form_class = EditProfileForm
@@ -147,7 +114,7 @@ class BlogView(ListView):
     template_name = 'blog/home.html'
     ordering = ['-post_date']
     context_object_name = "StudentBlogModel_list"    #default is object_list as well as model's_verbose_name_list and/or model's_verbose_name_plural_list, if defined in the model's inner Meta class
-    paginate_by = 10  
+    paginate_by = 10
     def get_context_data(self, *args,**kwargs):
         context = super(BlogView, self).get_context_data(**kwargs)
         context['page_user'] = Profile.objects.all()
@@ -163,8 +130,6 @@ class AddBlogView(CreateView):
     model = StudentBlogModel
     form_class = Blog_Post_Form
     template_name = 'blog/add_blog_post.html'
-    # fields = '__all__'
-    # fields = ('title','body')
 
 class UpdateBlogPostView(UpdateView):
     model = StudentBlogModel
@@ -185,14 +150,11 @@ class ResourcesView(ListView):
     template_name = 'resources/resources.html'
     ordering = ['title']
     context_object_name = "ResorcesModel_list"    #default is object_list as well as model's_verbose_name_list and/or model's_verbose_name_plural_list, if defined in the model's inner Meta class
-    paginate_by = 10 
 
 class AddResourcesView(CreateView):
     model = ResorcesModel
     form_class = ResorcesModelForm
     template_name = 'resources/add_resources.html'
-    # fields = '__all__'
-    # fields = ('title','body')
 
 class ResourcesDetailView(DetailView):
     model = ResorcesModel
@@ -200,21 +162,21 @@ class ResourcesDetailView(DetailView):
 
 def OnCampusView(request):
     data = Placement_Company_Detail.objects.filter(job_type='On-Campus').order_by('-post_date')
-    paginator = Paginator(data, 8) 
+    paginator = Paginator(data, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'app/OnCampusJob.html',context={'page_obj':page_obj})
 
 def OffCampusView(request):
     data = Placement_Company_Detail.objects.filter(job_type='Off-Drive').order_by('-post_date')
-    paginator = Paginator(data, 8) 
+    paginator = Paginator(data, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'app/OffCampusJob.html',context={'page_obj':page_obj})
 
 def Internship(request):
     data = Placement_Company_Detail.objects.filter(job_type='Internship').order_by('-post_date')
-    paginator = Paginator(data, 8) 
+    paginator = Paginator(data, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'app/Internship.html',context={'page_obj':page_obj})
