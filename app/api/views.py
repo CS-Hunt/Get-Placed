93f752c676b8587a9ab1bae80e175c3077d64564
Nitesh Singh <ns98738698@gmail.com>
2021-11-20 14:44:59 +0000

    Added Compiler Design Resources

--- app/api/views.py
+++ app/api/views.py
@@ -9,15 +9,18 @@ class ResourcesModelAPI(viewsets.ModelViewSet):
     serializer_class =  ResorcesSerializer
     filter_backends = [OrderingFilter]
     ordering = ['title']
+    http_method_names = ['get']
 
 class JobAPI(viewsets.ModelViewSet):
     queryset = Placement_Company_Detail.objects.all()
     serializer_class =  Placement_Company_DetailAPI
     filter_backends = [OrderingFilter]
     ordering = ['-post_date']
+    http_method_names = ['get']
 
 class BlogAPI(viewsets.ModelViewSet):
     queryset = StudentBlogModel.objects.all()
     serializer_class =  Blog_Post_FormAPI
     filter_backends = [OrderingFilter]
     ordering = ['-post_date']
+    http_method_names = ['get']
